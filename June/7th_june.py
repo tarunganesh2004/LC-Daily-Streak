90ebf295b9bc9870df36a763c894a03dd0c4fff1
tarunganesh2004 <enstarunganesh@gmail.com>
2026-06-07 08:08:54 +0530

    done 7th june

---
+++ June/7th_june.py
@@ -0,0 +1,50 @@
+# LC 2196 Create Binary Tree From Descriptions
+
+class TreeNode:
+    def __init__(self,val,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right 
+
+
+class Solution:
+    def createBinaryTree(self,descriptions):
+        # root is the node that never appears as a child
+        nodes={}
+        children=set()
+        for parent,child,isLeft in descriptions:
+            if parent not in nodes:
+                nodes[parent]=TreeNode(parent)
+
+            if child not in nodes:
+                nodes[child]=TreeNode(child)
+
+            if isLeft:
+                nodes[parent].left=nodes[child]
+            else:
+                nodes[parent].right=nodes[child]
+
+            children.add(child)
+
+        for node_val in nodes:
+            if node_val not in children:
+                return nodes[node_val]
+
+
+# preorder traversal
+def preorder(root):
+    if not root:
+        return
+
+    print(root.val, end=" ")
+    preorder(root.left)
+    preorder(root.right)
+
+
+descriptions=[[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+
+sol=Solution()
+
+root=sol.createBinaryTree(descriptions)
+
+preorder(root)
